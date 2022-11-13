74baf9082d8de26dbdb19ebfc147dc501297a9dd
Ethan Huang <yehuang2020@gmail.com>
2022-11-13 17:58:23 -0500

    added new interaction in Sketch.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, {Component} from "react";
-import Sketch from "./Sketches/Sketch";
+import Grid from "./Sketches/Sketch";
 import "./App.css"; 
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      predictions: []
+      key: null
     }
-    
+  
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key == "1"){
+      this.setState(prevState => ({
+        ...prevState,
+        key: event.key
+      }))
+    }
+
+    if (event.key == "2"){
+      this.setState(prevState => ({
+        ...prevState,
+        key: event.key
+      }))
+    }
   }
 
   render(){
     return(
-      <div className = "centered">
-        <Sketch/>
-      </div>
+          <div className = "centered">
+            <input type="text" id="one" onKeyPress={this.handleKeyPress} />
+            <h1>Option 1</h1>
+            <Grid option = {this.state.key}/>
+          </div>
     )
   }
 }
--- src/Sketches/Sketch.js
+++ src/Sketches/Sketch.js
@@ -5,7 +5,9 @@ import * as ml5 from "ml5";
 let video; 
 let cnv; 
 let handpose; 
-let predictions = []; 
+let facemesh; 
+let predictionsFinger = []; 
+let predictionsFace = []; 
 let position = [0,0]; 
 let grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,18 +23,39 @@ let grid = [
 ]; 
 let numFingers = 0; 
 
-export default (props) => {
+//face
+let moveLeftPrev = false; 
+let moveLeft = false; 
+let leftSwitch = false; 
+let moveRightPrev = false; 
+let moveRight = false; 
+let rightSwitch = false; 
+let moveDownPrev = false; 
+let moveDown = false; 
+let downSwitch = false; 
+let moveUpPrev = false; 
+let moveUp = false; 
+let upSwitch = false; 
+
+function Grid(props){
     
 	const setup = (p5, canvasParentRef) => {
         video = p5.createCapture(p5.VIDEO);
         video.size(p5.windowWidth, p5.windowHeight);
         handpose = ml5.handpose(video, modelReady);
         handpose.on("predict", results => {
-            predictions = results;
-          });
+            predictionsFinger = results;
+        });
+
+        facemesh = ml5.facemesh(video, modelReady);
+        facemesh.on("predict", results => {
+            predictionsFace = results;
+        });
+        
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
 		cnv = p5.createCanvas(700, 700).parent(canvasParentRef);
+        video.hide()
 	};
 
 
@@ -41,22 +64,21 @@ export default (props) => {
     }
 
 	const draw = (p5) => {
-        p5.createCanvas(1000,1000); 
-        p5.background(0); 
-
+        p5.createCanvas(p5.windowWidth, p5.windowHeight); 
+        p5.background(15,16,22); 
         configureGrid();
         drawGrid(p5); 
+        p5.textSize(32)
     }
 
+    //for finding fingers and the number of fingers being held up 
     function findFingers() {
         let fingersUp = 0; 
-        console.log(predictions.length)
-        for (let i = 0; i < predictions.length; i += 1) {
-            const prediction = predictions[i];
+        for (let i = 0; i < predictionsFinger.length; i += 1) {
+            const prediction = predictionsFinger[i];
 
             //position of tip and base of fingers
-            const thumbTip = prediction.landmarks[4]
-            const thumbBase = prediction.landmarks[5]
+            const center = prediction.landmarks[0]
 
             const indexTip = prediction.landmarks[8]
             const indexBase = prediction.landmarks[5]
@@ -70,56 +92,129 @@ export default (props) => {
             const pinkyTip = prediction.landmarks[20]
             const pinkyBase = prediction.landmarks[17]
 
-            /*
-            if (distance([thumbTip[0], thumbTip[1]], [thumbBase[0], thumbBase[1]])){
+            let iFull = distance([indexTip[0], indexTip[1]], [center[0], center[1]])
+            let iHalf = distance([indexBase[0], indexBase[1]], [center[0], center[1]])
+
+            let mFull = distance([middleTip[0], middleTip[1]], [center[0], center[1]])
+            let mHalf = distance([middleBase[0], middleBase[1]], [center[0], center[1]])
+
+            let rFull = distance([ringTip[0], ringTip[1]], [center[0], center[1]])
+            let rHalf = distance([ringBase[0], ringBase[1]], [center[0], center[1]])
+
+            let pFull = distance([pinkyTip[0], pinkyTip[1]], [center[0], center[1]])
+            let pHalf = distance([pinkyBase[0], pinkyBase[1]], [center[0], center[1]])
+
+            if (iFull >= iHalf){
                 fingersUp += 1
             }
-            */
-            if (distance([indexTip[0], indexTip[1]], [indexBase[0], indexBase[1]])){
+            if (mFull >= mHalf){
                 fingersUp += 1
             }
-            if (distance([middleTip[0], middleTip[1]], [middleBase[0], middleBase[1]])){
+            if (rFull >= rHalf){
                 fingersUp += 1
             }
-            if (distance([ringTip[0], ringTip[1]], [ringBase[0], ringBase[1]])){
-                fingersUp += 1
-            }
-            if (distance([pinkyTip[0], pinkyTip[1]], [pinkyBase[0], pinkyBase[1]])){
+            if (pFull >= pHalf){
                 fingersUp += 1
             }
 
             numFingers = fingersUp
-            console.log(fingersUp)
+            console.log(numFingers)
         }
     }
 
+    function findFace(){
+        for (let i = 0; i<predictionsFace.length; i++){
+            let points = predictionsFace[i].scaledMesh; 
+    
+            for (let j= 0; j<points.length; j++){
+                let x = points[j][0]; 
+                let y = points[j][1]; 
+    
+                let right = points[127][0]
+                let middle = points[6]
+                let left = points[356][0]
+                let lip_up = points[13]
+                let lip_down = points[14]
+                let up = points[151]
+                let down = points[17]
+                moveRightPrev = moveRight 
+                moveLeftPrev = moveLeft
+                moveUpPrev = moveUp
+                moveDownPrev = moveDown
+
+                if (isNegative(right, middle[0])){
+                    console.log("Right")
+                    moveRight = true; 
+                    if (moveRightPrev){
+                        rightSwitch = false; 
+                    }
+                    else if (!moveRightPrev){
+                        rightSwitch = true; 
+                        if (position[0] <= grid.length-2){
+                            position[0] += 1; 
+                        }
+                    }
+    
+                }else{
+                    moveRight = false; 
+                    rightSwitch = false; 
+                }
+
+                if (!isNegative(left, middle[0])){
+                    moveLeft = true; 
+                    if (moveLeftPrev){
+                        leftSwitch = false; 
+                    }
+                    else if (!moveLeftPrev){
+                        leftSwitch = true; 
+                        position[0] -= 1; 
+                        if (position[0]<=0){
+                        position[0] = 0; 
+                    }
+                }
+                }else{
+                    moveLeft = false; 
+                    leftSwitch = false; 
+                }
+            }
+        }
+                
+
+    }
+
+
     function configureGrid(){
-        findFingers()
-        if (numFingers == 1){
-            grid[position[0]][position[1]] = 1
+        if (props.option == 1){
+            findFingers(); 
+            if (numFingers == 1){
+                grid[position[0]][position[1]] = 1
+            }
+
+            if (numFingers == 2){
+                grid[position[0]][position[1]] = 2
+            }
+
+            if (numFingers == 3){
+                grid[position[0]][position[1]] = 3
+            }
+
+            if (numFingers == 4){
+                grid[position[0]][position[1]] = 0 
+            }
         }
 
-        if (numFingers == 2){
-            grid[position[0]][position[1]] = 2
+        if (props.option == 2){
+            findFace()
         }
-
-        if (numFingers == 3){
-            grid[position[0]][position[1]] = 3
-        }
-
-        if (numFingers == 4){
-            grid[position[0]][position[1]] = 0 
-        }
-
     }
 
     function drawGrid(p5) {
         var size = 50; 
-		p5.background(255);
+        p5.stroke(255); 
         for(var i = 0; i <grid.length; i++){
         for (var j = 0; j< grid.length; j++){
           if (grid[i][j] == 0){
-              p5.fill(255,255,255);  
+              p5.fill(15, 16, 22);  
           }
           else if (grid[i][j] == 1) {
               p5.fill(61,92,123); 
@@ -130,27 +225,26 @@ export default (props) => {
           else if (grid [i][j] == 3) {
               p5.fill(226,121,130); 
           }
-          p5.rect(25 + (size*i) , 25 + (size*j), size, size); 
+          p5.rect(500 + (size*i) , 25 + (size*j), size, size); 
       }
-      p5.fill(0); 
-      p5.rect(25 + (size*position[0]) , 25 + (size*position[1]), size, size);
+      p5.fill(255); 
+      p5.rect(500 + (size*position[0]) , 25 + (size*position[1]), size, size);
     }
 	}
-        
 
     function distance(p1, p2) {
-        //let dis = Math.sqrt(Math.pow(p2[1] - p1[1], 2) +  Math.pow(p2[0] - p1[0], 2))
-        let dis = (p1[1] - p2[1])
-        console.log(dis)
-        if (dis > 0){
-            return false
-        }else{
-            return true
+        let dis = Math.sqrt(Math.pow(p2[1] - p1[1], 2) +  Math.pow(p2[0] - p1[0], 2))
+        return dis 
+    }
+
+    function isNegative(one, two){
+        if (two - one <0){
+            return true; 
         }
+        return false; 
     }
 
   const keyPressed = (p5) =>{
-    console.log(p5.keyCode)
     if (p5.keyCode == 37){
       if (position[0] >= 1){
         position[0] -= 1
@@ -174,3 +268,5 @@ export default (props) => {
   }
 	return <Sketch setup={setup} draw={draw} keyPressed ={keyPressed} />;
 };
+
+export default Grid
